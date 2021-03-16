c94a841b70bfe7a65068bb4142bba28b18e7ff30
eungang3 <eungang3@naver.com>
2021-03-16 12:09:51 +0900

    added categories

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.category_name}"
