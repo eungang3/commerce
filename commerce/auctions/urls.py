326f1ffb21331f71f642cfabb7400dab7d7aba77
eungang3 <eungang3@naver.com>
2021-03-24 21:10:17 +0900

    styling each listing page

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("all", views.get_all_items, name="get_all_items"),
     path("listings/<str:catname>/", views.get_items),
-    path("listings/<str:catname>/<str:listingid>/", views.get_listing),
+    path("listings/<str:catname>/<str:listingid>/", views.get_listing, name="get_listing"),
+    path("bid", views.bid, name="place_bid")
 ]
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,6 +3,8 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.forms import ModelForm
+from django import forms
 
 from .models import *
 
@@ -77,8 +79,34 @@ def get_all_items(request):
         "listings" : Listing.objects.all()
     })
 
+# create the form class
+class BiddingForm(ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['price']
+        widgets = {
+            'price': forms.NumberInput(attrs={'placeholder': '입찰가를 입력하세요'}),
+        }
+
 # for rendering each listing page
 def get_listing(request, catname, listingid):
     return render(request, "auctions/listing.html", {
-        "listing": Listing.objects.filter(id=listingid)
+        "listing": Listing.objects.filter(id=listingid).first(),
+        "current_price": "5000",
+        "bidding_form": BiddingForm()
     })
+
+# when user places a bid, save it in database and redirect
+def bid(request):
+    if request.method == "POST":
+        form = BiddingForm(request.POST)
+        if form.is_valid():
+            price = form.cleaned_data['price']
+        return HttpResponseRedirect(reverse("index"))
+
+    else:
+        return render(request, "auctions/listing.html", {
+            "listing": Listing.objects.filter(id=listingid).first(),
+            "current_price": "5000",
+            "bidding_form": BiddingForm()
+        })
